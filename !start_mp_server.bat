3c5812f98aef9cc8ecb81e5d4bdcd6a636f54784
Chase Hall <c@chse.xyz>
2021-05-31 15:26:42 -0500

    General Update
    + batch files will use variables now, similar to other games
    + server.cfg changed to match style of other games

---
+++ !start_mp_server.bat
@@ -0,0 +1,23 @@
+@echo off
+::Paste the server key from https://platform.plutonium.pw/serverkeys here
+set key=placeServerKeyHere
+::Name of the config file the server should use. (default: server.cfg)
+set cfg=server.cfg
+::Name of the server shown in the title of the cmd window. This will NOT bet shown ingame.
+set name=TDM 1
+::Port used by the server (default: 27016)
+set port=27016
+::Only change this when you don't want to keep the bat files in the game folder. MOST WON'T NEED TO EDIT THIS!
+set gamepath=%cd%
+
+title PlutoniumIW5 - %name% - Server restarter
+echo Visit plutonium.pw / Join the Discord (a6JM2Tv) for NEWS and Updates!
+echo Server "%name%" will load %cfg% and listen on port %port% UDP!
+echo To shut down the server close this window first!
+echo (%date%)  -  (%time%) %name% server start.
+
+cd /D %LOCALAPPDATA%\Plutonium
+:server
+start /wait /abovenormal bin\plutonium-bootstrapper-win32.exe iw5mp "%gamepath%" -dedicated +set key %key% +set sv_config %cfg% +net_port %port% +start_map_rotate
+echo (%date%)  -  (%time%) WARNING: %name% server closed or dropped... server restarts.
+goto server
---
+++ !updatePluto.bat
@@ -0,0 +1,2 @@
+@echo off
+plutonium.exe -update-only
