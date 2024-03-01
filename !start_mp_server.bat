d3c31805d02b6f6ce9d928d34e55d6b5ef78a7a8
Xerxes <5236639+xerxes-at@users.noreply.github.com>
2024-03-01 19:27:08 +0100

    Add mod support to batch file.
    Co-Authored-By: JezuzLizard <26215347+jezuzlizard@users.noreply.github.com>

--- !start_mp_server.bat
+++ !start_mp_server.bat
@@ -7,6 +7,8 @@ set cfg=server.cfg
 set name=TDM 1
 ::Port used by the server (default: 27016)
 set port=27016
+::Mod name (default "")
+set mod=""
 ::Only change this when you don't want to keep the bat files in the game folder. MOST WON'T NEED TO EDIT THIS!
 set gamepath=%cd%
 
@@ -18,6 +20,6 @@ echo (%date%)  -  (%time%) %name% server start.
 
 cd /D %LOCALAPPDATA%\Plutonium
 :server
-start /wait /abovenormal bin\plutonium-bootstrapper-win32.exe iw5mp "%gamepath%" -dedicated +set key %key% +set sv_config %cfg% +net_port %port% +start_map_rotate
+start /wait /abovenormal bin\plutonium-bootstrapper-win32.exe iw5mp "%gamepath%" -dedicated +set key %key% +set fs_game %mod% +set sv_config %cfg% +net_port %port% +start_map_rotate
 echo (%date%)  -  (%time%) WARNING: %name% server closed or dropped... server restarts.
 goto server
